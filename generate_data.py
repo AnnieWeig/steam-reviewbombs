4c6e646888499e7cbd7f58ba5c5e8d6465de033a
AnnieWeig <155455414+AnnieWeig@users.noreply.github.com>
2026-04-18 18:08:55 +0200

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# fake dataset (replace with your real data)
+#Test data -> Replace with Data and Preprocessing
 df = pd.DataFrame({
     "x": np.random.uniform(-1, 1, 300),
     "y": np.random.uniform(-1, 1, 300),
@@ -9,9 +9,7 @@ df = pd.DataFrame({
     "value": np.random.rand(300)
 })
 
-# example preprocessing (filter + normalize)
 df = df[df["value"] > 0.2]
 
 # export to JSON for AR
 df.to_json("data.json", orient="records")
-print("data.json created")
